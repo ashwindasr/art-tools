37a5088569b80362137eac1f372cb06729010b4c
Ashwin Das <ashwindasr@gmail.com>
2025-09-26 17:59:23 -0400

    test

--- pyartcd/pyartcd/pipelines/build_oadp.py
+++ pyartcd/pyartcd/pipelines/build_oadp.py
@@ -61,8 +61,6 @@ class BuildOadpPipeline:
                 assembly=self.assembly,
                 group=self.group,
                 operator_nvrs=["oadp-operator-container-1.3.8-202509262014.p2.g25ddcb5.assembly.test.el9"],
-                doozer_data_path=self._doozer_env_vars["DOOZER_DATA_PATH"] or '',
-                doozer_data_gitref='',
             )
         except Exception as e:
             self._logger.exception(f"Failed to trigger bundle build: {e}")
